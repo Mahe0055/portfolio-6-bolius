10689e2f362f054e4ddafea7375b6323be0b2f55
Amalie <Amal956e@stud.kea.dk>
2022-12-01 11:00:22 +0100

    leafletjs

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-dgsdg
+//Geographical coordinates
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-vvvvvvv
+//OpenStreetMap
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//Marker
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
