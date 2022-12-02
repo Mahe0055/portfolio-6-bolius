073d0d24e32a944d7a5ec60a3041eddd18a3f8e8
Amalie <Amal956e@stud.kea.dk>
2022-12-02 10:38:11 +0100

    Radio buttons viser daginst..

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+console.log(geojsonInst.features);
+
+function displayCorrectArrows() {
+    L.geoJSON(geojsonInst).addTo(map);
+}
+
 //Geographical coordinates
 var map = L.map('map').setView([55.676098, 12.568337], 11);
 
@@ -8,8 +14,24 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //Tilføjer daginstitutioner på kortet
-L.geoJSON(geojsonInst).addTo(map);
+//L.geoJSON(geojsonInst).addTo(map);
 
 //Tilføjer gårdhaver kortet
-L.geoJSON(geojsonGarden).addTo(map);
+//L.geoJSON(geojsonGarden).addTo(map);
+
+// Radio buttons der gør at daginstitutioner bliver vist
+const daycareRadioButton = document.querySelector("#day-careCentre");
+daycareRadioButton.addEventListener("click",function(event){
+    L.geoJSON(geojsonInst).addTo(map);
+});
+
+// Radio buttons der gør at gårdhaver bliver vist
+const backGardenRadioButton = document.querySelector("#backGarden");
+daycareRadioButton.addEventListener("click",function(event){
+    L.geoJSON(geojsongarden).addTo(map);
+});
+
+
+
+
 
