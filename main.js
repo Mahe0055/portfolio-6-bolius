238d5b1371d12f7ab84f661a6cbbba59220fae70
Mahe0055 <mahe00555@stud.kea.dk>
2022-12-05 08:46:19 +0100

    map and filter in box

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ daycareRadioButton.addEventListener("click", function (event) {
 const backGardenRadioButton = document.querySelector("#backGarden");
 backGardenRadioButton.addEventListener("click", function (event) {
     L.geoJSON(geojsonGarden).addTo(map);
-
 });
 
 // Vis alle "Radio button" der gør at daginstitutioner og gårdhaver bliver vist på kortet samtidig
@@ -25,14 +24,19 @@ const showAllRadioButton = document.querySelector("#showAll");
 showAllRadioButton.addEventListener("click", function (event) {
     L.geoJSON(geojsonInst).addTo(map);
     L.geoJSON(geojsonGarden).addTo(map);
-
 });
 
 // Nulstil "button" der gør at daginstitutioner og gårdhaver bliver fjernet på kortet
 const clearAllRadioButton = document.querySelector("#clearAll");
 clearAllRadioButton.addEventListener("click", function (event) {
     // Der skal stå noget her for at det bliver fjernet
-
+    for (let i = 0; i < geojsonInst.length; i++ ) {
+        geojsonInst[i].setMap(null);
+    }
 });
+
+
+
+
 
 
