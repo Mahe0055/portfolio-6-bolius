899c48936fd2931649ccd132b2b426c95fd2a05b
Amalie <Amal956e@stud.kea.dk>
2022-12-06 09:58:36 +0100

    Mangler gårshaver - ny farve

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Daginstitutioner "Radio button" der gør at daginstitutioner bliver vist på kortet
 const daycareRadioButton = document.querySelector("#day-careCentre");
 daycareRadioButton.addEventListener("click", function (event) {
+// Daginstitutioner - runde grønne cirkler
     L.geoJSON(geojsonInst, {
         pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng, geojsonMarkerOptions);
@@ -20,25 +21,35 @@ daycareRadioButton.addEventListener("click", function (event) {
 // Gårdhaver "Radio button" der gør at gårdhaver bliver vist på kortet
 const backGardenRadioButton = document.querySelector("#backGarden");
 backGardenRadioButton.addEventListener("click", function (event) {
-    L.geoJSON(geojsonGarden).addTo(map);
+// Gårdhaver - runde farvede cirkler
+    L.geoJSON(geojsonGarden, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptionsG);
+        }
+    }).addTo(map);
 });
 
 // Vis alle "Radio button" der gør at daginstitutioner og gårdhaver bliver vist på kortet samtidig
 const showAllRadioButton = document.querySelector("#showAll");
 showAllRadioButton.addEventListener("click", function (event) {
-
+// Daginstitutioner - runde grønne cirkler
     L.geoJSON(geojsonInst, {
         pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng, geojsonMarkerOptions);
         }
     }).addTo(map);
-    L.geoJSON(geojsonGarden).addTo(map);
+// Gårdhaver - runde farvede cirkler
+    L.geoJSON(geojsonGarden, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptionsG);
+        }
+    }).addTo(map);
 });
 
 // Nulstil "button" der gør at daginstitutioner og gårdhaver bliver fjernet på kortet
 const clearAllRadioButton = document.querySelector("#clearAll");
 clearAllRadioButton.addEventListener("click", function (event) {
-        document.location.reload();
+    document.location.reload();
 });
 
 
