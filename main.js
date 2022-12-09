458f4131a24ba21223afd78da8866cb72c048a56
Amalie <Amal956e@stud.kea.dk>
2022-12-09 09:16:43 +0100

    Sat const i toppen og gjort at knappen forsvinder, når man tryller på "Nulstil"

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-//Geographical coordinates
-var map = L.map('map').setView([55.676098, 12.568337], 11);
+//Geografiske koordinater i København
+let map = L.map('map').setView([55.676098, 12.568337], 11.5);
+
+//HTML Knapper
+const daycareRadioButton = document.querySelector("#day-careCentre"); //"Daginstitutioner" knap
+const backGardenRadioButton = document.querySelector("#backGarden"); //"Gårdhaver" knap
+const showAllRadioButton = document.querySelector("#showAll"); //"Vis alle" knap
+const clearAllRadioButton = document.querySelector("#clearAll"); //"Nulstil" knap
+
+//Gårdhave styling - mørke grønne "firkanter" på kortet
+const myStyle =
+    {
+        weight: 1,
+        opacity: 100,
+        color: "#3E5959",
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
 
 //OpenStreetMap
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,22 +23,19 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Daginstitutioner "Radio button" der gør at daginstitutioner bliver vist på kortet
-const daycareRadioButton = document.querySelector("#day-careCentre");
+//Daginstitutioner "Radio button" der gør at daginstitutioner bliver vist på kortet
 daycareRadioButton.addEventListener("click", function (event) {
-//Finder alle datapunkter med tagget "gaardhaver" og fjerner dem
+    //Finder alle datapunkter med tagget "gaardhaver" og fjerner dem, da gårdhaver ikke skal vises, når man trykker på daginstitutioner
     map.eachLayer(function (layer) {
         if (layer.myTag === "gaardhaver") {
             map.removeLayer(layer)
         }
-
     });
 
-//Tilføjer et tag på hver daginstitutions datapunkt
     L.geoJSON(geojsonInst, {
         onEachFeature: function (feature, layer) {
-            layer.myTag = "daginstitutioner";
-            layer.bindPopup(feature.properties.enhedsnavn); //Tilføjer "enhedsnavn" på hver grøn cirkel
+            layer.myTag = "daginstitutioner"; //Tilføjer et tag på hver daginstitutions datapunkt
+            layer.bindPopup(feature.properties.enhedsnavn + " " + "<br>" + feature.properties.vejadresseringsnavn); //Tilføjer "enhedsnavn" på hver grøn cirkel
         },
         pointToLayer: function (feature, latlng) {//Daginstitutioner - runde grønne cirkler
             return L.circleMarker(latlng, geojsonMarkerOptions);
@@ -30,81 +43,58 @@ daycareRadioButton.addEventListener("click", function (event) {
     }).addTo(map);
 });
 
-
-// Gårdhaver "Radio button" der gør at gårdhaver bliver vist på kortet
-const backGardenRadioButton = document.querySelector("#backGarden");
+//Gårdhaver "Radio button" der gør at gårdhaver bliver vist på kortet
 backGardenRadioButton.addEventListener("click", function (event) {
-// Gårdhaver
-    const myStyle =
-        {
-            weight: 1,
-            opacity: 100,
-            color: "#3E5959",
-            dashArray: '3',
-            fillOpacity: 0.7
-        };
-
-//Finder alle datapunkter med tagget "daginstitutioner" og fjerner dem
+    //Finder alle datapunkter med tagget "daginstitutioner" og fjerner dem, da daginstitutioner ikke skal vises, når man trykker på gårdhaver
     map.eachLayer(function (layer) {
         if (layer.myTag === "daginstitutioner") {
             map.removeLayer(layer)
         }
     });
 
-//Tilføjer et tag på hver gårdhave datapunkt
     L.geoJSON(geojsonGarden, {
         style: myStyle,
         onEachFeature: function (feature, layer) {
-            layer.myTag = "gaardhaver"
-            layer.bindPopup(feature.properties.karrenavn); //Tilføjer "karrenavn" på hver orange firkant
+            layer.myTag = "gaardhaver" //Tilføjer et tag på hver gårdhave datapunkt
+            layer.bindPopup(feature.properties.karrenavn); //Tilføjer "karrenavn" på hver gårdhave, så når man trykker, ser man navnet på gårdhaven
         }
     }).addTo(map);
 });
 
-
-// Vis alle "Radio button" der gør at daginstitutioner og gårdhaver bliver vist på kortet samtidig
-const showAllRadioButton = document.querySelector("#showAll");
+//Vis alle "Radio button" der gør at daginstitutioner og gårdhaver bliver vist på kortet samtidig
 showAllRadioButton.addEventListener("click", function (event) {
-// Tilføjer et tag på hver daginstitutions datapunkt
     L.geoJSON(geojsonInst, {
         onEachFeature: function (feature, layer) {
-            layer.myTag = "daginstitutioner"
-            layer.bindPopup(feature.properties.enhedsnavn); //Tilføjer "enhedsnavn" på hver grøn cirkel
+            layer.myTag = "daginstitutioner" //Tilføjer et tag på hver daginstitutions datapunkt
+            layer.bindPopup(feature.properties.enhedsnavn); //Tilføjer "enhedsnavn" på hver grøn cirkel, så når man trykker, vises navnet på daginstitutionen
         },
-        pointToLayer: function (feature, latlng) {//Daginstitutioner - runde grønne cirkler
+        pointToLayer: function (feature, latlng) { //Daginstitutioner - runde grønne cirkler
             return L.circleMarker(latlng, geojsonMarkerOptions);
         }
     }).addTo(map);
 
-//Gårdhaver - flade lysegrønne mærkninger
-    const myStyle =
-        {
-            weight: 1,
-            opacity: 100,
-            color: "#3E5959",
-            dashArray: '3',
-            fillOpacity: 0.7
-        };
-
-//Tilføjer et tag på hver gårdhave datapunkt
     L.geoJSON(geojsonGarden, {
         style: myStyle,
         onEachFeature: function (feature, layer) {
-            layer.myTag = "gaardhaver"
-            layer.bindPopup(feature.properties.karrenavn); //Tilføjer "karrenavn" på hver orange firkant
+            layer.myTag = "gaardhaver" //Tilføjer et tag på hver gårdhave datapunkt
+            layer.bindPopup(feature.properties.karrenavn); //Tilføjer "karrenavn" på hver gårdhave, så når man trykker, ser man navnet på gårdhaven
         }
     }).addTo(map);
 });
 
 //Nulstil "button" der gør at daginstitutioner og gårdhaver bliver fjernet på kortet
-const clearAllRadioButton = document.querySelector("#clearAll");
 clearAllRadioButton.addEventListener("click", function (event) {
-   //Fjerner daginstitutioner og gårdhaver layers, når man trykker på nulstil
+    //Fjerner daginstitutioner og gårdhaver layers, når man trykker på nulstil
     map.eachLayer(function (layer) {
         if (layer.myTag) {
             map.removeLayer(layer)
         }
     });
     map.setView([55.676098, 12.568337], 11); //Når man trykker på "nulstil" kommer kortet tilbage til udgangspunktet
+
+    //Fjerner daginstitutioner og gårdhaver layers, når man trykker på nulstil
+    daycareRadioButton.checked = false
+    backGardenRadioButton.checked = false
+    showAllRadioButton.checked = false
 });
 
